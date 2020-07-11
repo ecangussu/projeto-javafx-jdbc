00e22be378fd27e8a30fa195061497ad03ac563e
Estevao Souza <estevaosouza18@gmail.com>
2020-07-11 16:41:00 -0300

    Adição de comentários no código

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,11 +13,18 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			//Instanciação da tela para poder manipula-la antes de usa-la
+			//Passa o caminho da view
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			//Carregar a view
 			Parent parent = loader.load();
+			//Instanciação da cena passando como parametro o objeto principal da view (AnchorPane da view) 
 			Scene mainScene = new Scene(parent);
+			//Palco da cena tendo como cena principal o mainScene
 			primaryStage.setScene(mainScene);
+			//Título do palco
 			primaryStage.setTitle("Aplicação JavaFX");
+			//Mostrar o palco
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
